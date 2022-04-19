dbe0a158eeddeb75afb974170fdc25699ca521a8
Dayana <dayanaazzam99@gmail.com>
2022-04-20 02:45:58 +0300

    Addition of functions

--- models/BaseModel.php
+++ models/BaseModel.php
@@ -1,48 +1,82 @@
 <?php
 
 namespace Models;
-
+use PDO;
+use Utility\Database;
 abstract class BaseModel {
   public $pdo;
   public $id;
   public $TABLE_NAME;
 
-  public function __construct($db) {
-    $this->pdo = $db;
+  public function __construct() {
+    $this->TABLE_NAME = $this->getTableName();
+    $this->pdo = Database::getConnection();
   }
 
   abstract function getTableName();
-
+  
  
    public function getAll() {
-      $sql = "SELECT * FROM " . $this->$TABLE_NAME . "";
+      $sql = "SELECT * FROM " . $this->TABLE_NAME;
       $stmt = $this->pdo->prepare($sql);
       $stmt->execute();
       $rowCount = $stmt->rowCount();
-      if($rowCount > 0){
+      
         $data = $stmt->fetchAll();
-        $result= json_encode(['success' => 1,'data' => $data,]);
-    }
-    else{
-      $result= json_encode(array("message" => "No record found."));
-   }
-   return  $result;
+        
+   
+   return  $data;
   }
 
   public function getByID(int $id){
 
+    $sql = "SELECT * FROM " . $this->TABLE_NAME . "WHERE id=?";
+    $stmt = $this->pdo->prepare($sql);
+    $stmt->execute();
+    $rowCount = $stmt->rowCount();
+    
+      $data = $stmt->fetchAll();
+     
+  
+  return  $data;
   }
 
   public function create(array $data){
+    $column = implode(", ",array_keys($data));
+    $value  = implode(", ", array_values($data));
+    $sql = "INSERT ". $this->TABLE_NAME ."($column) VALUES('$value')";
+    $stmt = $this->pdo->prepare($sql);
 
+    $this->column=htmlspecialchars(strip_tags($this->column));
+
+    $stmt->bindParam("$value", $this->column,$this->pdo=PDO::PARAM_STR);
+
+    $record= $stmt->execute();
+
+  return $record; 
   }
 
-  public function update(int $id, array $data){
 
+  public function update(int $id, array $data){
+    $column = implode(", ",array_keys($data));
+    $value  = implode(", ", array_values($data));
+    $sql = "UPDATE". $this->TABLE_NAME ."SET $column= :".$value." WHERE id=?";
+    $stmt = $this->pdo->prepare($sql);
+
+    $this->column=htmlspecialchars(strip_tags($this->column));
+
+    $stmt->bindParam(':'.$value, $this->column,$this->pdo=PDO::PARAM_STR);
+
+    $record= $stmt->execute();
+  
+  return $record; 
   } 
 
   public function delete(int $id){
-
+    $sql = "DELETE * FROM " . $this->TABLE_NAME . "WHERE id=?";
+    $stmt = $this->pdo->prepare($sql);
+    $stmt->execute();
+  return  $stmt;
   }
 
 }
--- models/Employee.php
+++ models/Employee.php
@@ -17,48 +17,35 @@ class Employee extends BaseModel {
   }
 
   public function getAll() {
-  //   $sql = "SELECT * FROM " . $this->TABLE_NAME . "";
-  //   $stmt = $this->pdo->prepare($sql);
-  //   $stmt->execute();
-  //   $employeeCount = $stmt->rowCount();
-  //   if($employeeCount > 0){
-  //     $data = $stmt->fetchAll();
-  //     $result= json_encode(['success' => 1,'data' => $data,]);
-  // }
-
-  // else{
-  //    $result= json_encode(array("message" => "No record found."));
-  // }
-  // return  $result;  
-  }
-
+    return parent::getAll(); 
+    json_encode(['success' => 1,'data' => $data,]);
+    }
+    
+ 
   public function getByID($id){
-
+    return parent::getBYID();
+    $result= json_encode(['success' => 1,'data' => $data,]);
   }
 
   public function create(array $data){
-    // $sql = "INSERT ". $this->db_table ."(firstname,gender) VALUES(:firstname,:gender)";
-
-    // $stmt = $this->pdo->prepare($sql);
-
-    // $this->firstname=htmlspecialchars(strip_tags($this->firstname));
-    // $this->gender=htmlspecialchars(strip_tags($this->gender));  
-
-    // $stmt->bindParam(":firstname", $this->firstname,$this->pdo=PDO::PARAM_STR);
-    // $stmt->bindParam(":gender", $this->gender,$this->pdo=PDO::PARAM_STR); 
-
-    // if($stmt->execute()){
-    //     return true;
-    //  }
-    //  return false;
-
+    return parent::create();
+    echo json_encode([
+      'status' => 'ok',
+      'message' => 'Employee created successfully.'
+  ]);
   }
 
   public function update(int $id, array $data){
-
+    return parent::update();
+    echo json_encode([
+      'status' => 'ok',
+      'message' => 'Employee updated successfully.'
+  ]);
   }
 
   public function delete(int $id){
-
+    return parent::delete();
+    $result= json_encode(['status' => 'ok',
+      'message' => 'Employee deleted successfully.']);
   }
 }
