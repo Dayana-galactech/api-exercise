fd83fb643578a780ba62c37648f7cd42e75c0e3d
Dayana <dayanaazzam99@gmail.com>
2022-04-19 13:29:14 +0300

    baseModel getAll function

--- models/BaseModel.php
+++ models/BaseModel.php
@@ -3,11 +3,46 @@
 namespace Models;
 
 abstract class BaseModel {
+  public $pdo;
+  public $id;
+  public $TABLE_NAME;
 
-  public function __construct() {
-
+  public function __construct($db) {
+    $this->pdo = $db;
   }
 
   abstract function getTableName();
+
+ 
+   public function getAll() {
+      $sql = "SELECT * FROM " . $this->$TABLE_NAME . "";
+      $stmt = $this->pdo->prepare($sql);
+      $stmt->execute();
+      $rowCount = $stmt->rowCount();
+      if($rowCount > 0){
+        $data = $stmt->fetchAll();
+        $result= json_encode(['success' => 1,'data' => $data,]);
+    }
+    else{
+      $result= json_encode(array("message" => "No record found."));
+   }
+   return  $result;
+  }
+
+  public function getByID(int $id){
+
+  }
+
+  public function create(array $data){
+
+  }
+
+  public function update(int $id, array $data){
+
+  } 
+
+  public function delete(int $id){
+
+  }
 
 }
--- models/Employee.php
+++ models/Employee.php
@@ -1,15 +1,64 @@
 <?php
 
 namespace Models;
-
+use PDO;
 class Employee extends BaseModel {
-  const TABLE_NAME = 'employees';
+  const TABLE_NAME = 'employee';
+  public $firstname;
+  public $gender;
+ 
 
-  public function __construct() {
-    parent::__construct();
+  public function __construct($db) {
+    parent::__construct($db);
   }
 
-  function getTableName() {
+  public function getTableName() {
     return self::TABLE_NAME;
+  }
+
+  public function getAll() {
+  //   $sql = "SELECT * FROM " . $this->TABLE_NAME . "";
+  //   $stmt = $this->pdo->prepare($sql);
+  //   $stmt->execute();
+  //   $employeeCount = $stmt->rowCount();
+  //   if($employeeCount > 0){
+  //     $data = $stmt->fetchAll();
+  //     $result= json_encode(['success' => 1,'data' => $data,]);
+  // }
+
+  // else{
+  //    $result= json_encode(array("message" => "No record found."));
+  // }
+  // return  $result;  
+  }
+
+  public function getByID($id){
+
+  }
+
+  public function create(array $data){
+    // $sql = "INSERT ". $this->db_table ."(firstname,gender) VALUES(:firstname,:gender)";
+
+    // $stmt = $this->pdo->prepare($sql);
+
+    // $this->firstname=htmlspecialchars(strip_tags($this->firstname));
+    // $this->gender=htmlspecialchars(strip_tags($this->gender));  
+
+    // $stmt->bindParam(":firstname", $this->firstname,$this->pdo=PDO::PARAM_STR);
+    // $stmt->bindParam(":gender", $this->gender,$this->pdo=PDO::PARAM_STR); 
+
+    // if($stmt->execute()){
+    //     return true;
+    //  }
+    //  return false;
+
+  }
+
+  public function update(int $id, array $data){
+
+  }
+
+  public function delete(int $id){
+
   }
 }
